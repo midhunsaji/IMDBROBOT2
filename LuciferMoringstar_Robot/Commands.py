2cac1fcd45364d1efc574d138df5c9a72c805e83
midhunsaji <89266093+midhunsaji@users.noreply.github.com>
2021-10-01 15:41:02 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -119,7 +119,7 @@ async def start(bot, message):
             reply_markup=InlineKeyboardMarkup(
                 [[
                 InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                InlineKeyboardButton("More Botz", url="https://t.me/MT_Botz")
+                InlineKeyboardButton("Join Channel", url="https://t.me/MoviePlayr")
                 ],[
                 InlineKeyboardButton("Help", callback_data="help"),
                 InlineKeyboardButton("About", callback_data="about")
@@ -334,7 +334,7 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('More Botz', url='https://t.me/MT_Botz'),
+            InlineKeyboardButton('Join Channel', url='https://t.me/MoviePlayr'),
             InlineKeyboardButton('Video', url=f'{TUTORIAL}')
         ]
         ]
